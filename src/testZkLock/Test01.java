5a50ae59697e577bf84afce090633ae0a1fcbd51
leibailong <11632lbl>
2019-06-11 14:27:02 +0800

    练习

--- src/testZkLock/Test01.java
+++ src/testZkLock/Test01.java
@@ -6,8 +6,8 @@ import java.util.Date;
 
 public class Test01 {
 
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) throws Exception{
+        /*for (int i = 0; i < 10; i++) {
             new Thread(()->{
                 SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
 
@@ -15,6 +15,9 @@ public class Test01 {
 
                 System.err.println("生成的订单号是：" + orderNo);
             }).start();
-        }
+        }*/
+        long requestStartTime = System.currentTimeMillis();
+        Thread.sleep(1000);
+        System.out.println("本次请求共用=====>" + ((System.currentTimeMillis() - requestStartTime)/ 1000 % 60) + " 秒");
     }
 }
